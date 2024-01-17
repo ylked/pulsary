f97e3ef908f700b9ec590b4f92df9f0cecf5e6b8
ylked <nima@dekhli.ch>
2024-01-17 09:02:35 +0100

    add initial spring project

---
+++ src/PulsaryAPI/src/main/java/ch/hearc/nde/pulsaryapi/PulsaryApiApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.nde.pulsaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsaryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulsaryApiApplication.class, args);
+    }
+
+}
