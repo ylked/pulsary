273951893d42b0355ff647614129b4782e280ece
ylked <nima@dekhli.ch>
2024-01-28 00:23:17 +0100

    add methods to user repository

--- src/PulsaryAPI/src/main/java/ch/hearc/nde/pulsaryapi/repository/UserRepository.java
+++ src/PulsaryAPI/src/main/java/ch/hearc/nde/pulsaryapi/repository/UserRepository.java
@@ -1,2 +1,12 @@
-package ch.hearc.nde.pulsaryapi.repository;public class UserRepository {
+package ch.hearc.nde.pulsaryapi.repository;
+
+import ch.hearc.nde.pulsaryapi.model.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<UserEntity,Long> {
+    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByToken(String token);
+
 }
