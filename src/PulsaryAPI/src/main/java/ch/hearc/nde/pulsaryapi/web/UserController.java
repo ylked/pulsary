9f41cc49f862862b94530ab87b108157f6bef184
ylked <nima@dekhli.ch>
2024-01-28 00:24:58 +0100

    implement login and register in user controller

---
+++ src/PulsaryAPI/src/main/java/ch/hearc/nde/pulsaryapi/web/UserController.java
@@ -0,0 +1,53 @@
+package ch.hearc.nde.pulsaryapi.web;
+
+import ch.hearc.nde.pulsaryapi.UserRegistrationForm;
+import ch.hearc.nde.pulsaryapi.model.UserEntity;
+import ch.hearc.nde.pulsaryapi.repository.UserRepository;
+import ch.hearc.nde.pulsaryapi.service.FailedLoginException;
+import ch.hearc.nde.pulsaryapi.service.UnavailableUsernameException;
+import ch.hearc.nde.pulsaryapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@Controller
+public class UserController {
+    private @Autowired UserService service;
+
+    @PostMapping("/users")
+    public @ResponseBody ResponseEntity<UserEntity> create_user(@RequestBody UserRegistrationForm form) {
+        try {
+            UserEntity user = service.create(form);
+            return ResponseEntity.created(
+                    ServletUriComponentsBuilder
+                            .fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(user.getId())
+                            .toUri()).build();
+
+        } catch (UnavailableUsernameException e) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT).build();
+
+        }
+    }
+
+    @PostMapping("/login")
+    public @ResponseBody ResponseEntity<UserEntity> login(@RequestBody UserRegistrationForm form) {
+        try {
+            UserEntity user = service.login(form);
+            return ResponseEntity.ok(user);
+
+        } catch (FailedLoginException e) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED).build();
+
+        }
+    }
+}
