2d884a76190b630bcf0699633ce0886214eabd92
ylked <nima@dekhli.ch>
2024-01-28 02:10:40 +0100

    change security dependency

--- src/PulsaryAPI/src/main/java/ch/hearc/nde/pulsaryapi/config.java
+++ src/PulsaryAPI/src/main/java/ch/hearc/nde/pulsaryapi/config.java
@@ -4,21 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class config {
 
-    @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests(authz -> authz
-                        .anyRequest().permitAll()
-                );
-        return http.build();
-    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
